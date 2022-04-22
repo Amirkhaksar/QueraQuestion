3b96f79be401588614ece6268538c28f8ed1c565
AmirHosseinKhaskar <84659761+Amirkhaksar@users.noreply.github.com>
2022-04-22 17:17:41 +0430

    BobAndTv

---
+++ BobAndTv/BobAndTv.py
@@ -0,0 +1,20 @@
+#Language:python
+#ID:14580
+#QLink:https://quera.org/problemset/14580/
+#Author:Amirkhaksar
+
+inp = input().split()
+n,i,k = inp[0],inp[1],inp[2]
+name_network = []
+for d in range(0,int(n)):
+     inp = input()
+     name_network.append(inp)
+for j in range(0,int(k)):
+     i = (int(i)+1) % int(n)
+print(name_network[i-1])
+
+    
+
+
+
+
