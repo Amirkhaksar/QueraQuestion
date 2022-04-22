6622545d8d15fb207b6e7023c036410b43587bdd
pegahghaffari <102922850+pegahghaffari@users.noreply.github.com>
2022-04-22 17:12:52 +0430

    Calculate the selling price of fleece

---
+++ MrPashmal/MrPashmal.cpp
@@ -0,0 +1,17 @@
+/*
+Language:C++
+ID:72877
+QLink:https://quera.org/problemset/72877/
+Author:pegahghafari
+*/
+#include<iostream>
+using namespace std;
+int main()
+{
+	int n,p,s;
+	float k;
+	cin>>n>>k>>p;
+	s=n*k*p;
+	cout<<s;
+	return 0;
+}
