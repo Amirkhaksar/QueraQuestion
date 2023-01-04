5ccf27dbf914d8d3b8dfc5634452c661ce9d850b
Alireza Shojaei <93857562+LancelotMilton@users.noreply.github.com>
2023-01-05 02:04:50 +0330

    Create ProfessionalChess.c

---
+++ ProfessionalChess/ProfessionalChess.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main()
+{
+	int chessPieces[6];
+	scanf("%d %d %d %d %d %d", &chessPieces[0], &chessPieces[1],&chessPieces[2],&chessPieces[3],&chessPieces[4],&chessPieces[5]); 
+	printf("%d %d %d %d %d %d",1 - chessPieces[0],1 - chessPieces[1],2 - chessPieces[2],2 - chessPieces[3],2 - chessPieces[4],8 - chessPieces[5]);
+}
